c9eea71eefb595ed8d21bf19e5fa13a353547dd1
pongkarm <darunpoppong@gmail.com>
2024-12-13 22:34:44 +0700

    EX

--- week4/teach_sort/EX_Bubble-sort.py
+++ week4/teach_sort/EX_Bubble-sort.py
@@ -3,10 +3,14 @@ a = [4, 7, 9, 2, 6, 8, 9, 4, 7, 9, 2, 6, 8, 9, 1]
 def Bubble_sort(a_list):
     n = len(a_list)
     for i in range(n):
+        flag = 0
         for j in range(0, n-i-1):
             #swap
             if a_list[j] > a_list[j+1] :
                 a_list[j], a_list[j+1] = a_list[j+1], a_list[j]
+                flag = 1
+        if flag == 0:
+            break
     return a_list
 
 print(a)
---
+++ week4/teach_sort/EX_Selection_Sort.py
@@ -0,0 +1,18 @@
+def Selection_sort(a_list):
+    n = len(a_list)
+    for i in range(n):
+        min_index = i
+        for j in range(i + 1, n):
+            if a_list[j] < a_list[min_index]:
+                min_index = j
+        a_list[i], a_list[min_index] = a_list[min_index], a_list[i]
+
+    return a_list
+
+a = [4, 7, 9, 2, 6, 8, 9, 4, 7, 9, 2, 6, 8, 9, 1]
+sorted_list = Selection_sort(a)
+print("Sorted list:", sorted_list)
+
+print(a)
+print(Selection_sort(a))
+
