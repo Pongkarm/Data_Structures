a7e17c148a2da1d2fea33b9df88f4362a018342e
pongkarm <darunpoppong@gmail.com>
2024-12-13 17:04:57 +0700

    add code EX_Bubble-sort

---
+++ week4/teach_sort/EX_Bubble-sort.py
@@ -0,0 +1,13 @@
+a = [4, 7, 9, 2, 6, 8, 9, 4, 7, 9, 2, 6, 8, 9, 1]
+
+def Bubble_sort(a_list):
+    n = len(a_list)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            #swap
+            if a_list[j] > a_list[j+1] :
+                a_list[j], a_list[j+1] = a_list[j+1], a_list[j]
+    return a_list
+
+print(a)
+print(Bubble_sort(a))
