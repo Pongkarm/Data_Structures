a502b605c747355cf6c3fd0a5af0c3098d2ec5d6
Kong19565 <khaogong48@gmail.com>
2024-12-13 22:54:37 +0700

    Add Ex Timer sort

---
+++ week4/teach_sort/EX_Insertion_Sort.py
@@ -0,0 +1,18 @@
+def Insection_sort(a_list):
+    n = len(a_list)
+    for i in range(1, n):
+        temp = a_list[i]
+        hole = i
+        while hole > 0 and a_list[hole - 1] > temp:
+            a_list[hole] = a_list[hole - 1]
+            hole -= 1
+        a_list[hole] = temp
+
+    return a_list
+
+a = [4, 7, 9, 2, 6, 8, 9, 4, 7, 9, 2, 6, 8, 9, 1]
+sorted_list = Insection_sort(a)
+print("Sorted list:", sorted_list)
+
+print(a)
+print(Insection_sort(a))
