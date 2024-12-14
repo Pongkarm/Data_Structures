8cd62549d2bd73a4090aa36f5ecec8e0934ebf7d
Kong19565 <khaogong48@gmail.com>
2024-12-14 17:49:28 +0700

    update file Split

--- week4/Split_amphoe.py
+++ week4/Split_amphoe.py
@@ -1,3 +1,5 @@
+import timeit
+start_time = timeit.default_timer()
 #this file is split file amphoe to txt
 import re
 def read_sql_file_and_process(file_path):
@@ -9,16 +11,19 @@ def read_sql_file_and_process(file_path):
 
 # รายชื่อไฟล์ที่ต้องการประมวลผล
 files = [
-    "amphoe.sql"
+    "week4/amphoe.sql"
 ]
 
 for file_path in files:
     data = read_sql_file_and_process(file_path)
     print(f"Processed data from {file_path}:")
     for row in data[:5]:  # แสดงข้อมูล 5 บรรทัดแรกเพื่อดูตัวอย่าง
-        print(row)
+        print(row[0], row[3])
     print("\n")
     
-with open('amphoe.txt', 'w', encoding='utf-8') as write_province:
+with open('week4/amphoe.txt', 'w', encoding='utf-8') as write_province:
   for row in data:
-    write_province.write(f"'{row[0]}' '{row[1]}' '{row[2]}' '{row[3]}'\n")
+    write_province.write(f"{row[0]} {row[3]}\n")
+
+elapsed = timeit.default_timer() - start_time
+print(f"{elapsed} seconds")
--- week4/Split_tambol.py
+++ week4/Split_tambol.py
@@ -1,3 +1,5 @@
+import timeit
+start_time = timeit.default_timer()
 #this file is split file tambol to txt
 import re
 def read_sql_file_and_process(file_path):
@@ -10,16 +12,19 @@ def read_sql_file_and_process(file_path):
 
 # รายชื่อไฟล์ที่ต้องการประมวลผล
 files = [
-    "tambol.sql"
+    "week4/tambol.sql"
 ]
 
 for file_path in files:
     data = read_sql_file_and_process(file_path)
     print(f"Processed data from {file_path}:")
     for row in data[:5]:  # แสดงข้อมูล 5 บรรทัดแรกเพื่อดูตัวอย่าง
-        print(row)
+        print(row[0], row[1])
     print("\n")
     
-with open('tambol.txt', 'w', encoding='utf-8') as write_province:
+with open('week4/tambol.txt', 'w', encoding='utf-8') as write_province:
   for row in data:
-    write_province.write(f"{row[0]} {row[1]} {row[2]} {row[3]} {row[4]} {row[5]}\n")
+    write_province.write(f"{row[0]} {row[1]}\n")
+
+elapsed = timeit.default_timer() - start_time
+print(f"{elapsed} seconds")
