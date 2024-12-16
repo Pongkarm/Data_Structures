feb059267affe50fb91d9d90831ae65fbc814424
pongkarm <darunpoppong@gmail.com>
2024-12-16 19:20:26 +0700

    recursive function

--- week4/Testcode.py
+++ week4/Testcode.py
@@ -1,12 +1,24 @@
-a = [4, 7, 9, 2, 6, 8, 9, 4, 7, 9, 2, 6, 8, 9, 1]
+# a = [4, 7, 9, 2, 6, 8, 9, 4, 7, 9, 2, 6, 8, 9, 1]
 
-def Bubble_sort(a_list):
-    n = len(a_list)
-    while True:    
-        if a_list[j] > a_list[j+1] :
-            a_list[j], a_list[j+1] = a_list[j+1], a_list[j]
-            flag = 1
-    return a_list
+# def Bubble_sort(a_list):
+#     n = len(a_list)
+#     while True:    
+#         if a_list[j] > a_list[j+1] :
+#             a_list[j], a_list[j+1] = a_list[j+1], a_list[j]
+#             flag = 1
+#     return a_list
 
-print(a)
-print(Bubble_sort(a))
+# print(a)
+# print(Bubble_sort(a))
+
+def pongkarm(a, sum = 0):
+    new = a*a-1
+    sum = sum + new
+    if a != 1:
+        a -= 1
+        print(sum, a)
+        pongkarm(a, sum)
+    else:
+        print('end')
+        
+pongkarm(5)
